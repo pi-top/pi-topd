bee7389c0d68554859c5893a2e9916940e21298c
Ben P <ben@pi-top.com>
2017-08-10 14:42:35 +0100

    Send logging output to the journal

--- src/ptdm_logger.py
+++ src/ptdm_logger.py
@@ -1,15 +1,45 @@
+import logging
+from systemd.journal import JournalHandler
+
+class Logger:
+
+	def __init__(self, logging_level):
+
+		self._logging_level = logging_level
+		self._journal_log = logging.getLogger("pt-device-manager")
+
+		self._journal_log.addHandler(JournalHandler())
+		self._journal_log.setLevel(self._logging_level)
+		self._journal_log.info("Logging started.")
 
 
-def debug(message):
-	
-	print (message)
+	def debug(self, message):
+		
+		if (self._logging_level <= 20):
+			print (message)
+
+		self._journal_log.debug(message)
 
 
-def info(message):
-	
-	print (message)	
+	def info(self, message):
+		
+		if (self._logging_level <= 20):
+			print (message)
+		
+		self._journal_log.info(message)
 
 
-def error(message):
-	
-	print (message)		
+	def warning(self, message):
+		
+		if (self._logging_level <= 20):
+			print (message)
+		
+		self._journal_log.warning(message)
+
+
+	def error(self, message):
+		
+		if (self._logging_level <= 20):
+			print (message)
+		
+		self._journal_log.error(message)
