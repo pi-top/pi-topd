57cf41f64c2fb0bec8017af22c00b8e410d556f8
Ben P <ben@pi-top.com>
2017-08-10 16:02:24 +0100

    Add some messaging functions to the publish server

--- src/ptdm_publish_server.py
+++ src/ptdm_publish_server.py
@@ -4,6 +4,7 @@
 
 import zmq
 import time
+import ptdm_messages
 
 _logger = None
 _zmq_context = None
@@ -41,8 +42,65 @@ def stop_listening():
 	_logger.debug ("Done.")
 
 
-def send_message(message):
+def publish_brightness_changed(new_brightness):
 
-    _logger.debug ("Publishing message: " + message)
-	    
-    _zmq_socket.send_text(message)
+	_logger.debug ("Publishing brightness changed")
+	_send_message(ptdm_messages.PUB_BRIGHTNESS_CHANGED, [ new_brightness ])
+
+
+def publish_peripheral_connected(peripheral_id):
+
+	_logger.debug ("Publishing peripheral connected")
+	_send_message(ptdm_messages.PUB_PERIPHERAL_CONNECTED, [ peripheral_id ])
+
+
+def publish_peripheral_disconnected(peripheral_id):
+
+	_logger.debug ("Publishing peripheral disconnected")
+	_send_message(ptdm_messages.PUB_PERIPHERAL_DISCONNECTED, [ peripheral_id ])
+
+
+def publish_shutdown_requested():
+
+	_logger.debug ("Publishing shutdown requested")
+	_send_message(ptdm_messages.PUB_SHUTDOWN_REQUESTED, [ ])
+
+
+def publish_reboot_required():
+
+	_logger.debug ("Publishing reboot required")
+	_send_message(ptdm_messages.PUB_REBOOT_REQUIRED, [ ])
+
+
+def publish_battery_charging_state_changed(connected_int):
+
+	_logger.debug ("Publishing battery charging state changed")
+
+	_send_message(ptdm_messages.PUB_BATTERY_CHARGING_STATE_CHANGED, [ connected_int ])
+
+
+def publish_battery_capacity_changed(new_capacity):
+
+	_logger.debug ("Publishing battery capacity changed")
+	_send_message(ptdm_messages.PUB_BATTERY_CAPACITY_CHANGED, [ new_capacity ])
+
+
+def publish_battery_time_remaining_changed(new_time):
+
+	_logger.debug ("Publishing battery time remaining changed")
+	_send_message(ptdm_messages.PUB_BATTERY_TIME_REMAINING_CHANGED, [ new_time ])
+
+
+# Internal functions
+
+def _send_message(message_id, parameters):
+
+	message_to_send = str(message_id)
+
+	for message_param in parameters:
+		message_to_send += "|"
+		message_to_send += str(message_param)
+
+	_logger.debug ("Publishing message: " + message_to_send)
+		
+	_zmq_socket.send_string(message_to_send)
