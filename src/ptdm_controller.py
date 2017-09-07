e4efa9619c7fcfd577523fd5e1f57596928396f3
Ben P <18192544+benp44@users.noreply.github.com>
2017-09-07 17:41:49 +0100

    Respond to screen blanking timeout messages, and save timeout to config file

--- src/ptdm_controller.py
+++ src/ptdm_controller.py
@@ -106,6 +106,12 @@ class Controller():
     def _on_request_get_peripheral_enabled(self, peripheral_id):
         return self._peripheral_manager.get_peripheral_enabled(peripheral_id)
 
+    def _on_request_get_screen_blanking_timeout(self):
+        return self._idle_monitor.get_configured_timeout()
+
+    def _on_request_set_screen_blanking_timeout(self, timeout):
+        return self._idle_monitor.set_configured_timeout(timeout)
+
     ###########################################
     # Idle Monitor callback methods
     ###########################################
