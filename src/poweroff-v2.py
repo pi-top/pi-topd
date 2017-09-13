47d0df0c8202f935560ee66acdee8e402e9ffc24
Ben P <18192544+benp44@users.noreply.github.com>
2017-09-13 15:02:03 +0100

    Move i2cdevice into common python lib

--- src/poweroff-v2.py
+++ src/poweroff-v2.py
@@ -4,7 +4,7 @@
 # is launched by systemd responding to halt.target/poweroff.target, to
 # ensure the hub shuts down after the raspberry pi
 
-from pthub2 import pthub2_i2c_device
+from ptcommon import i2c_device
 
 PWR__SHUTDOWN_CTRL = 0xA0
 PWR__M3_TIMEOUT = 0xAE
@@ -13,21 +13,21 @@ PWR__SHUTDOWN_CTRL__MODE2 = 0x16
 
 try:
 
-    i2c_device = pthub2_i2c_device.I2CDevice("/dev/i2c-1", 0x10, None)
-    i2c_device.connect()
+    hub = i2c_device.I2CDevice("/dev/i2c-1", 0x10, None)
+    hub.connect()
 
     # Set the timeout on the mode 3 shutdown
-    i2c_device.write_word(PWR__M3_TIMEOUT, 15)
+    hub.write_word(PWR__M3_TIMEOUT, 15)
 
     # Get the current power control register
-    shutdown_control = i2c_device.read_unsigned_byte(PWR__SHUTDOWN_CTRL)
+    shutdown_control = hub.read_unsigned_byte(PWR__SHUTDOWN_CTRL)
 
     # Set the shutdown mode to 3
     shutdown_control = shutdown_control | PWR__SHUTDOWN_CTRL__MODE1
     shutdown_control = shutdown_control | PWR__SHUTDOWN_CTRL__MODE2
 
-    # Write back to the device
-    i2c_device.write_byte(PWR__SHUTDOWN_CTRL, shutdown_control)
+    # Write back to the hub
+    hub.write_byte(PWR__SHUTDOWN_CTRL, shutdown_control)
 
 except Exception as e:
 
