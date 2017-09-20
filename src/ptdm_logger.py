5de867f575a8b23dfadbf73167ae1f5f0a35a78d
Ben P <18192544+benp44@users.noreply.github.com>
2017-09-20 11:29:10 +0100

    Use command line args for the logging parameters

--- src/ptdm_logger.py
+++ src/ptdm_logger.py
@@ -4,46 +4,39 @@
 import inspect
 import logging
 from systemd.journal import JournalHandler
+import datetime
 
 
 class Logger:
 
-    def __init__(self, logging_level):
+    log_level_indicators = {10: 'D', 20: 'I', 30: 'W', 40: 'E', 50: '!'}
+
+    def __init__(self, logging_level, log_to_journal):
 
         self._logging_level = logging_level
+        self._log_to_journal = log_to_journal
         self._journal_log = logging.getLogger("pt-device-manager")
-        self._journal_log.propagate = False
-
         self._journal_log.addHandler(JournalHandler())
         self._journal_log.setLevel(self._logging_level)
+
         self._journal_log.info("Logging started.")
 
-    def _print_message(self, message):
-        if (self._logging_level <= 10):
-            print(inspect.stack()[1][3] + ": " + message)
-        else:
-            print(message)
+    def _print_message(self, message, level):
+        if (self._log_to_journal is False and self._logging_level <= level):
+            print("[" + datetime.datetime.now().strftime("%H:%M:%S.%f") + " " + self.log_level_indicators[level] + "] " + message)
 
     def debug(self, message):
-        if (self._logging_level <= 10):
-            self._print_message(message)
-
+        self._print_message(message, logging.DEBUG)
         self._journal_log.debug(message)
 
     def info(self, message):
-        if (self._logging_level <= 20):
-            self._print_message(message)
-
+        self._print_message(message, logging.INFO)
         self._journal_log.info(message)
 
     def warning(self, message):
-        if (self._logging_level <= 30):
-            self._print_message(message)
-
+        self._print_message(message, logging.WARNING)
         self._journal_log.warning(message)
 
     def error(self, message):
-        if (self._logging_level <= 40):
-            self._print_message(message)
-
+        self._print_message(message, logging.ERROR)
         self._journal_log.error(message)
