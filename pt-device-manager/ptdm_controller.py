475742c496c5f5ad0e952e1e1ec57252d68b413d
Jorge Capona <jcapona@gmail.com>
2021-01-07 14:00:16 -0300

    Notify user when modifying SPI ports

--- pt-device-manager/ptdm_controller.py
+++ pt-device-manager/ptdm_controller.py
@@ -1,6 +1,9 @@
 # Instantiates and coordinates between the other classes
 from pitopcommon.logger import PTLogger
 from pitopcommon.common_ids import DeviceID
+from pitopcommon.command_runner import run_command
+
+from os import path
 from systemd.daemon import notify
 from time import sleep
 
@@ -191,6 +194,21 @@ class Controller:
             return 1
 
     def on_request_set_oled_spi_bus(self, spi_port_number):
+        def enable_spi_dtoverlay_if_required(spi_port_number):
+            PTLogger.info(f"Will use SPI port {spi_port_number}")
+            if spi_port_number == 0:
+                run_command("dtparam spi=on", timeout=5)
+                sleep(1)
+                if path.exists("/dev/spidev1.0"):
+                    self._notification_manager.display_old_spi_bus_still_active_message()
+            else:
+                run_command("dtparam spi=off", timeout=5)
+                if not path.exists("/dev/spidev1.0"):
+                    run_command(
+                        f"dtoverlay spi{spi_port_number}-1cs", timeout=5)
+                sleep(1)
+
+        enable_spi_dtoverlay_if_required(spi_port_number)
         self._hub_manager.set_oled_use_spi0(spi_port_number == 0)
 
     ###########################################
