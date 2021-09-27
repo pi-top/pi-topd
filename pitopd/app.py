89176a4d41175aa18f0416cc2d65137109f8f019
m-roberts <m-roberts@users.noreply.github.com>
2021-09-27 13:46:44 +0100

    self._pipe_manager

--- pitopd/app.py
+++ pitopd/app.py
@@ -64,13 +64,13 @@ class App:
             return False
 
         if self._hub_manager.connect_to_hub():
-            self.pipe_manager.set_hub_serial_number(
+            self._pipe_manager.set_hub_serial_number(
                 self._hub_manager._active_hub_module.get_serial_id()
             )
-            self.pipe_manager.set_battery_serial_number(
+            self._pipe_manager.set_battery_serial_number(
                 self._hub_manager._active_hub_module.get_battery_serial_number()
             )
-            self.pipe_manager.set_display_serial_number(
+            self._pipe_manager.set_display_serial_number(
                 self._hub_manager._active_hub_module.get_display_serial_id()
             )
             self._hub_manager.start()
